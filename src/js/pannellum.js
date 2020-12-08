e593a255a33010c53c93c5aa685d1c804fc4ff61
nkmnz <42554376+nkmnz@users.noreply.github.com>
2020-12-08 16:56:52 +0100

    Update pannellum.js
    set translateZ(0) so that webkit doesn't show the hotSpots when an overlay is shown

--- src/js/pannellum.js
+++ src/js/pannellum.js
@@ -1985,7 +1985,7 @@ function renderHotSpot(hs) {
         coord[0] += (canvasWidth - hs.div.offsetWidth) / 2;
         coord[1] += (canvasHeight - hs.div.offsetHeight) / 2;
         var transform = 'translate(' + coord[0] + 'px, ' + coord[1] +
-            'px) translateZ(9999px) rotate(' + config.roll + 'deg)';
+            'px) translateZ(0) rotate(' + config.roll + 'deg)';
         if (hs.scale) {
             transform += ' scale(' + (origHfov/config.hfov) / z + ')';
         }
